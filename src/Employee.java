ffab32003b015a7be7a1e69838711fd5dffbbc76
Longdinium <rondobell0303@gmail.com>
2023-11-25 21:50:45 +0900

    Employeeの更新

--- src/Employee.java
+++ src/Employee.java
@@ -5,6 +5,14 @@ public class Employee {
     public String divisionName; //部署名
     public int vitality; //体力
 
+    // コンストラクタ
+    public Employee() {
+        System.out.println("インスタンス化時にコンストラクタが呼ばれました");
+        employeeName = "未設定";
+        divisionName = "営業部";
+        vitality = 0;
+    }
+
     // 自己紹介メソッド
     public void introduce() {
         System.out.println("私の名前は" + employeeName + "です。");
